965d887e232de8fe81ec3504dbd1dbe8fb5cd657
meowmustang <tanmay.londhe.2029@gmail.com>
2026-01-19 10:33:33 +0530

    render frontend

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.enableCors({
-  origin: ["http://localhost:5173", "https://trackit-psi.vercel.app"],
+  origin: ["http://localhost:5173", "https://trackit-psi.vercel.app", "https://trackit-wdcb.onrender.com"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 })
